4878ed14ee9fe1fce4de613bfe191f59c71ab263
shivam797 <shivamkumar004667@gmail.com>
2023-07-09 13:56:29 +0530

    after rectifying db connection

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-// mongoose.connect(process.env.MONGO_URL);
-mongoose.connect('mongodb://0.0.0.0/googleauth');
+mongoose.connect(process.env.MONGO_URL);
+// mongoose.connect('mongodb://0.0.0.0/googleauth');
 
 const db = mongoose.connection;
 
